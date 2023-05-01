be2d7bc7074b6a53c38854f92dc98a45e1274acc
Jean <jeanants@icloud.com>
2023-05-01 11:07:19 -0300

    settings

--- src/JS/script.js
+++ src/JS/script.js
@@ -2,8 +2,11 @@ function ActivatedMenu() {
     let menuMobile = document.querySelector('.mobile-ul') 
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open')
+        document.querySelector('.icon').src = 'assets/images/menu-burger.png'
     } else {
         menuMobile.classList.add('open')
+        document.querySelector('.icon').src = 'assets/images/close-menu.png'
+
     }
 }
 
