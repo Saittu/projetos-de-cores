42fc3ff9357d072c9b556e87d1a4f503f4ea84cb
Jean <jeanants@icloud.com>
2023-05-02 22:34:12 -0300

    Ajustes e estilização nav-link

--- src/JS/script.js
+++ src/JS/script.js
@@ -2,10 +2,10 @@ function ActivatedMenu() {
     let menuMobile = document.querySelector('.mobile-ul') 
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open')
-        document.querySelector('.icon').src = 'assets/images/menu-burger.png'
+        document.querySelector('.icon').src = 'src/assets/images/menu-burger.png'
     } else {
         menuMobile.classList.add('open')
-        document.querySelector('.icon').src = 'assets/images/close-menu.png'
+        document.querySelector('.icon').src = 'src/assets/images/close-menu.png'
 
     }
 }
