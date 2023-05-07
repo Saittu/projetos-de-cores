ccbb0ad4757368b766ebdd222d071318464a05cc
Jean <jeanants@icloud.com>
2023-05-07 00:15:04 -0300

    adjusted

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,15 +1,17 @@
 function ActivatedMenu() {
     let menuMobile = document.querySelector('.mobile-ul') 
     let menuUl = menuMobile.querySelectorAll('a')
+    let iconNav = document.querySelector('.icon')
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open')
-        document.querySelector('.icon').src = 'src/assets/images/menu-burger.png'
+        iconNav.src = 'src/assets/images/menu-burger.png'
     } else {
         menuMobile.classList.add('open')
-        document.querySelector('.icon').src = 'src/assets/images/close-menu.png'
+        iconNav.src = 'src/assets/images/close-menu.png'
         menuUl.forEach((option) =>{
             option.addEventListener('click', () =>{
                 menuMobile.classList.remove('open')
+                iconNav.src = 'src/assets/images/menu-burger.png'
             })
         })
 
