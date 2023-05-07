df78c79d195219a07f3a8c1acafdac739fe3c24f
Jean <jeanants@icloud.com>
2023-05-07 00:11:35 -0300

    menu close click

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,11 +1,17 @@
 function ActivatedMenu() {
     let menuMobile = document.querySelector('.mobile-ul') 
+    let menuUl = menuMobile.querySelectorAll('a')
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open')
         document.querySelector('.icon').src = 'src/assets/images/menu-burger.png'
     } else {
         menuMobile.classList.add('open')
         document.querySelector('.icon').src = 'src/assets/images/close-menu.png'
+        menuUl.forEach((option) =>{
+            option.addEventListener('click', () =>{
+                menuMobile.classList.remove('open')
+            })
+        })
 
     }
 }
